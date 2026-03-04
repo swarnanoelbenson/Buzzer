6b2968b661b7cd4dae5780c2ba0a084ede6eddba
Noel Benson Swarna <swarnanoelbenson@gmail.com>
2026-03-04 22:06:59 +1100

    Initial Commit

---
+++ Buzzer/BuzzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuzzerApp.swift
+//  Buzzer
+//
+//  Created by Noel Benson on 4/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct BuzzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Buzzer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Buzzer
+//
+//  Created by Noel Benson on 4/3/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
